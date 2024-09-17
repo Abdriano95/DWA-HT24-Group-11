b6d5427faae2f00d5da92f66a1a3c55450f8397e
Joakim Olsson <165900249+joakimolssonn@users.noreply.github.com>
2024-09-17 14:04:00 +0200

    starting on merging the game with main-page

--- app.js
+++ app.js
@@ -107,6 +107,14 @@ function restart() {
     generateCards();
 }
 
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(page => {
+        page.style.display = 'none';
+    });
+    // Show the selected page
+    document.getElementById(pageId).style.display = 'block';
+}
+
 
 //Game menu
 function playGame() {
